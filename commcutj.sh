eb349b3f01923b86e2a81fef7f06c4f8ff3f1b12
pawhite <47261786+pawhite@users.noreply.github.com>
2019-02-03 17:22:50 -0500

    Add files via upload

--- commcutj.sh
+++ commcutj.sh
@@ -4,34 +4,10 @@ set path = ($path /usr/local/bin)
 
 cd /
 
-pkg install -y git
-sleep 180
-pkg install -y autoconf
-sleep 10
-pkg install -y automake
-sleep 12
-pkg install -y libtool
-sleep 3
-pkg install -y argtable
-sleep 5
-pkg install -y nano
-sleep 3
-pkg install -y ffmpeg
-sleep 180
-pkg install -y gcc7
-sleep 180
-pkg install -y handbrake
-sleep180
-
-pkg set -o devel/pkg-config:devel/pkgconf
-sleep 10
-pkg install -f devel/pkgconf
-sleep 10
-
-setenv CC gcc7
-
 mkdir /media/comchap
 mkdir /media/TSFiles
+
+setenv CC gcc7
 
 cd /usr/local 
 git clone --depth 1 https://github.com/erikkaashoek/Comskip
