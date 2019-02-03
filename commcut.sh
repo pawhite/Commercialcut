7c6081d79c98da8f1a55676cb3c14c8844c3a4da
pawhite <47261786+pawhite@users.noreply.github.com>
2019-02-03 11:18:04 -0500

    Add files via upload

--- commcut.sh
+++ commcut.sh
@@ -6,8 +6,8 @@ cd /
 mkdir /media/comchap
 mkdir /media/TSFiles
 
-yes | pkg set
-yes | pkg install
+yes | set
+yes | install
 
 pkg install autoconf
 pkg install automake
--- commcutj.sh
+++ commcutj.sh
@@ -4,8 +4,8 @@ set path = ($path /usr/local/bin)
 
 cd /
 
-yes | pkg set
-yes | pkg install
+yes | set
+yes | install
 
 mkdir /media/comchap
 mkdir /media/TSFiles
