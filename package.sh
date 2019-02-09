5bb8cc876ba2d499947751127aab6e2eed824975
pawhite <47261786+pawhite@users.noreply.github.com>
2019-02-09 07:06:17 -0500

    Add files via upload

--- package.sh
+++ package.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/csh
 
 set path = ($path /usr/local/bin)
 
 cd /
 
+#Install packages
 pkg install -y git
 pkg install -y bash
 pkg install -y autoconf
@@ -14,20 +15,25 @@ pkg install -y nano
 pkg install -y ffmpeg
 pkg install -y handbrake
 
+#Set up pkgconf and install
 echo y | pkg set -o devel/pkg-config:devel/pkgconf
 echo y | pkg install -f devel/pkgconf
 
+#Set compiler to use clang
 setenv CC clang
 
+#download Comskip and comchap from github 
 /usr/local/bin/git clone https://github.com/erikkaashoek/Comskip
 /usr/local/bin/git clone https://github.com/BrettSheleski/comchap
 
+#Compile comskip and download/add comskip.ini to guide the commercial cutting process
 cd /Comskip
 ./autogen.sh
 ./configure
 make
 fetch https://raw.githubusercontent.com/pawhite/Commercialcut/master/comskip.ini
 
+#Create a directory to place the original recording after commercial cutting and add the post-processing script
 cd /
 mkdir /media/ts_archive
 fetch https://raw.githubusercontent.com/pawhite/Commercialcut/master/post.sh
--- post.sh
+++ post.sh
@@ -2,21 +2,18 @@
 
 set path = ($path /usr/local/bin)
 
-set show = `basename "$1" .ts`
+set show = `basename "$1" .ts` 
 set grab = `printf "%s " $1:h`
 set a = `echo "$show".ts`
-set b = `echo "$grab/$show"_cut.ts`
+set b = `echo "$grab/$show"_cut.ts` 
 set c = `echo "$grab/$show".mkv`
 
-/comchap/comcut --ffmpeg=/usr/local/bin/ffmpeg --comskip=/Comskip/comskip --lockfile=/tmp/comchap.lock --comskip-ini=/Comskip/comskip.ini "$1" "$b"
+/comchap/comcut --ffmpeg=/usr/local/bin/ffmpeg --comskip=/Comskip-master/comskip --lockfile=/tmp/comchap.lock --comskip-ini=/Comskip-master/comskip.ini "$b" 
 
 /usr/local/bin/HandBrakeCLI --input "$b" --output "$c" --format mkv --encoder x264 --quality 18 --loose-anamorphic --decomb fast --x264-preset medium --h264-profile high --h264-level 4.1 --aencoder ac3 --mixdown 5point1
-#/usr/local/bin ffmpeg i "$1" -vf yadif -c:v libx264 -preset slow -crf 17 -c:a copy '/media/Recordings/"$show.mkv"'
 
 rm $b
 mv "$1" "/media/ts_archive/$a"
-
-
 
 
 
