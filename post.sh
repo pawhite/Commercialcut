716a1928170f65092c9e837903daf7c8186abf1f
pawhite <47261786+pawhite@users.noreply.github.com>
2019-02-09 08:49:18 -0500

    Add files via upload

--- post.sh
+++ post.sh
@@ -4,16 +4,15 @@ set path = ($path /usr/local/bin)
 
 set show = `basename "$1" .ts` 
 set grab = `printf "%s " $1:h`
-set a = `echo "$show".ts`
-set b = `echo "$grab/$show"_cut.ts` 
-set c = `echo "$grab/$show".mkv`
+set a = `echo "$grab/$show"_cut.ts` 
+set b = `echo "$grab/$show".mkv`
 
-/comchap/comcut --ffmpeg=/usr/local/bin/ffmpeg --comskip=/Comskip-master/comskip --lockfile=/tmp/comchap.lock --comskip-ini=/Comskip-master/comskip.ini "$b" 
+/comchap/comcut --ffmpeg=/usr/local/bin/ffmpeg --comskip=/Comskip-master/comskip --lockfile=/tmp/comchap.lock --comskip-ini=/Comskip-master/comskip.ini "$1" "$a" 
 
-/usr/local/bin/HandBrakeCLI --input "$b" --output "$c" --format mkv --encoder x264 --quality 18 --loose-anamorphic --decomb fast --x264-preset medium --h264-profile high --h264-level 4.1 --aencoder ac3 --mixdown 5point1
+/usr/local/bin/HandBrakeCLI --input "$a" --output "$b" --format mkv --encoder x264 --quality 18 --loose-anamorphic --decomb fast --x264-preset medium --h264-profile high --h264-level 4.1 --aencoder ac3 --mixdown 5point1
 
-rm $b
-mv "$1" "/media/ts_archive/$a"
+rm "$a"
+mv "$1" "/media/TSFiles/$show.ts"
 
 
 
