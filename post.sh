c9f80369d8d7470f08b96430d1ea0f0d7c65617e
pawhite <47261786+pawhite@users.noreply.github.com>
2019-02-08 19:46:12 -0500

    Add files via upload

--- post.sh
+++ post.sh
@@ -2,36 +2,21 @@
 
 set path = ($path /usr/local/bin)
 
-set filename = `printf "%s " $1:t:r`
+set show = `basename "$1" .ts`
 set grab = `printf "%s " $1:h`
-set a = `echo "$filename".ts`
-set b = `echo "$grab/$filename"_cut.ts`
-set c = `echo "$grab/$filename".mkv`
-sleep 10
+set a = `echo "$show".ts`
+set b = `echo "$grab/$show"_cut.ts`
+set c = `echo "$grab/$show".mkv`
 
-while (1)
-if ( ! -e "$b" ) then 
-echo "Running Comcut"
 /comchap/comcut --ffmpeg=/usr/local/bin/ffmpeg --comskip=/Comskip/comskip --lockfile=/tmp/comchap.lock --comskip-ini=/Comskip/comskip.ini "$1" "$b"
-echo "Comcut complete" 
-else
-echo "Comcut already done" 
-endif 
-break 
-end
 
-while (1)
-if ( ! `pgrep HandBrakeCLI` ) then 
-break 
-endif 
-echo "HandBrakeCLI is still running. Will try again in a minute." 
-sleep 60 
-end
+/usr/local/bin/HandBrakeCLI --input "$b" --output "$c" --format mkv --encoder x264 --quality 18 --loose-anamorphic --decomb fast --x264-preset medium --h264-profile high --h264-level 4.1 --aencoder ac3 --mixdown 5point1
+#/usr/local/bin ffmpeg i "$1" -vf yadif -c:v libx264 -preset slow -crf 17 -c:a copy '/media/Recordings/"$show.mkv"'
 
-HandBrakeCLI --input "$b" --output "$c" --format mkv --encoder x264 --quality 18 --loose-anamorphic --decomb fast --x264-preset medium --h264-profile high --h264-level 4.1 --aencoder ac3 --mixdown 5point1
-
-rm "$b"
+rm $b
 mv "$1" "/media/ts_archive/$a"
+
+
 
 
 
