1314919fe79274ebea179fc75e346048131daba4
pawhite <47261786+pawhite@users.noreply.github.com>
2019-02-10 19:16:01 -0500

    Add files via upload

--- post.sh
+++ post.sh
@@ -4,7 +4,7 @@ set path = ($path /usr/local/bin)
 
 set show = `basename "$1" .ts` 
 set grab = `printf "%s " $1:h`
-set a = `echo "$grab/$show"_cut.ts` 
+set a = `echo "$show"_cut.ts`
 set b = `echo "$grab/$show".mkv`
 
 /comchap/comcut --ffmpeg=/usr/local/bin/ffmpeg --comskip=/Comskip/comskip --lockfile=/tmp/comchap.lock --comskip-ini=/Comskip/comskip.ini "$1" "$a" 
