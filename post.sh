c05d0a479cfaff62d693d77d1b97938672863429
pawhite <47261786+pawhite@users.noreply.github.com>
2019-02-16 08:03:45 -0500

    Add files via upload

--- post.sh
+++ post.sh
@@ -6,7 +6,7 @@ set show = `basename "$1" .ts`
 set grab = `printf "%s " $1:h`
 set a = `echo "$grab/$show".mkv`
 
-/comchap/comcut --ffmpeg=/usr/local/bin/ffmpeg --comskip=/Comskip/comskip --lockfile=/tmp/comchap.lock --comskip-ini=/Comskip/comskip.ini "$1"
+comcut --ffmpeg=/usr/local/bin/ffmpeg --comskip=/usr/local/bin/comskip --lockfile=/tmp/comchap.lock --comskip-ini=/Comskip/comskip.ini "$1"
 
 /usr/local/bin/ffmpeg -i "$1" -vf yadif -c:v libx264 -preset slow -crf 18 -max_muxing_queue_size 1024 -c:a copy "$a"
 
